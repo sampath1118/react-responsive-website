e0a926cc83bcc300b7ac848c7e61cdec385523e6
sampii <sampath25318@gmail.com>
2021-06-28 22:28:46 +0530

    common code little bit changed

--- src/Common.jsx
+++ src/Common.jsx
@@ -10,12 +10,12 @@ const Common = (props) => {
             <div className="col-10 mx-auto">
               <div className="row">
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                  <h1 className="fw-bold">
+                  <h1 className="fw-bold text-capitalize">
                     {props.name}
                     <br />
                     <strong className="brand_name"> sampii</strong>
                   </h1>
-                  <h2 className="my-3 fs-5">
+                  <h2 className="my-3 fs-5 text-capitalize">
                     we are the team of talented developers making websites
                   </h2>
                   <div className="mt-3">
